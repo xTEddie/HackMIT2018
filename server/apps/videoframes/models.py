1053e94c7dd61b4888c8926f758243e2dfd369b9
xTEddie <edwardtranv1@gmail.com>
2018-09-16 00:16:39 -0400

    Fix typo

--- server/apps/videoframes/models.py
+++ server/apps/videoframes/models.py
@@ -11,7 +11,7 @@ class VideoFrame(models.Model):
     image = models.ImageField(upload_to=get_image_path, blank=False, null=False)
     uploaded_at = models.DateTimeField(blank=True, null=True)
     tag = models.ForeignKey('tags.tag', related_name='videoframes', on_delete=models.CASCADE, blank=True)
-    camera = models.ForeignKey('cameras.camera', related_name='cameras', on_delete=models.CASCADE, blank=True)
+    camera = models.ForeignKey('cameras.camera', related_name='videoframes', on_delete=models.CASCADE, blank=True)
 
     def save(self, *args, **kwargs): 
         if not self.id:
