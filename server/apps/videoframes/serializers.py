325c5ce7937e332b3edd3d39c792cbe878ee1ebf
xTEddie <edwardtranv1@gmail.com>
2018-09-16 03:33:12 -0400

    Display camera data

--- server/apps/videoframes/serializers.py
+++ server/apps/videoframes/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from apps.videoframes.models import VideoFrame
 from apps.tags.serializers import TagSerializer
+from apps.cameras.serializers import CameraSerializer
 
 
 class VideoFrameSerializer(serializers.ModelSerializer):
     tag = TagSerializer(read_only=True)
+    camera = CameraSerializer(read_only=True)
 
     class Meta:
         model = VideoFrame
